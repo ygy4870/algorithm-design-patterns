0cc6bb89dd99d3c00907c9914b63616b2edc9695
ygy4870 <1601541388@qq.com>
2020-09-20 20:36:51 +0800

    util

--- algorithm/src/main/java/com/ygy/study/algorithm/tree/TreeUtil.java
+++ algorithm/src/main/java/com/ygy/study/algorithm/tree/TreeUtil.java
@@ -1,76 +1,9 @@
 package com.ygy.study.algorithm.tree;
 
-import java.util.*;
+import java.util.List;
+import java.util.StringJoiner;
 
 public class TreeUtil {
-
-
-    public static TreeNode buildTree(int[] preorder, int[] inorder) {
-        return buildTreePreIn(preorder, 0, preorder.length-1, inorder, 0, inorder.length-1);
-    }
-
-
-
-    private static TreeNode buildTreePreIn(int[] preorder,int preStart, int preEnd, int[] inorder, int inStart, int inEnd) {
-        if (null==preorder || inorder==null) {
-            return null;
-        }
-        if (preStart==preEnd) {
-            return new TreeNode(preorder[preStart]);
-        }
-        int rootVal = preorder[preStart];
-
-        int i = inStart;
-        for (; i <= inEnd; i++) {
-            if (rootVal==inorder[i]) {
-                break;
-            }
-        }
-        int count = i-inStart;
-
-        TreeNode root = new TreeNode(rootVal);
-        root.left = buildTreePreIn(preorder, preStart+1,preStart+count, inorder, inStart, inStart+count-1);
-        root.right = buildTreePreIn(preorder, preStart+count+1,preEnd, inorder, inStart+count+1, inEnd);
-        return root;
-    }
-
-    public static TreeNode buildRandomTree(int depth, int valBound) {
-        if (depth <= 0) {
-            return null;
-        }
-        Random random = new Random();
-
-        TreeNode root = new TreeNode(random.nextInt(valBound));
-
-        Queue<TreeNode> treeNodes = new LinkedList<>();
-        treeNodes.offer(root);
-
-        TreeNode temp = null;
-        for (int i = 0; i < depth * 2 && !treeNodes.isEmpty(); i++) {
-
-            temp = treeNodes.poll();
-
-            if (randTrue(random)) {
-                TreeNode treeNode = new TreeNode(random.nextInt(valBound));
-                temp.setLeft(treeNode);
-                treeNodes.offer(treeNode);
-            }
-            if (randTrue(random)) {
-                TreeNode treeNode = new TreeNode(random.nextInt(valBound));
-                temp.setRight(treeNode);
-                treeNodes.offer(treeNode);
-            }
-
-        }
-
-
-        return root;
-    }
-
-    private static boolean randTrue(Random random) {
-        return random.nextInt(10) < 5 ? true : false;
-    }
-
 
     public static void main(String[] args) {
 //        TreeNode treeNode = buildRandomTree(6, 100);
@@ -84,7 +17,7 @@ public class TreeUtil {
         int[] preorder = {3,9,20,15,7};
         int[] inorder = {9,3,15,20,7};
 
-        TreeNode treeNode = buildTree(preorder, inorder);
+        TreeNode treeNode = TreeBuilder.buildTree(preorder, inorder);
         printPreOrder(treeNode);
         printInOrder(treeNode);
     }
